8081d96d24ae8bd117510ef9f466a643d0962d5e
Dipa_11 <dipamantre97@gmail.com>
2021-07-30 08:22:12 +0530

    two array using pointer file

---
+++ two_diam.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a[3][4]={
+		{1,2,3,4},
+		{5,6,7,8}'
+		{1,4,6,8}
+	};
+	int i,j;
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<4;j++)
+			printf("%d",a[i][j]);
+	}
+}
+
+
+	
